e73c77fe5b69062284216010efb7c47031172345
sevdaTan <68858980+sevdaTan@users.noreply.github.com>
2020-08-05 21:39:16 -0400

    Update GitFun.java

--- src/utility/GitFun.java
+++ src/utility/GitFun.java
@@ -3,7 +3,8 @@ package utility;
 public class GitFun {
     public static void main(String[] args) {
 
-        System.out.println("Everything is fun :) ");
+        System.out.println("Everything is fun :) Lots of fun!!!");
+        
 
     }
 }
